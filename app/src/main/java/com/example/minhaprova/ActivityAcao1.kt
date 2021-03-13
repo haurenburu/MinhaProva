474a3aa602a69f9e8d3a7dbc0c3460a756871c0c
haurenburu <haurenburu@protonmail.com>
2021-03-13 12:29:44 -0300

    ADD: questao2
    snack dont want to work

---
+++ app/src/main/java/com/example/minhaprova/ActivityAcao1.kt
@@ -0,0 +1,38 @@
+package com.example.minhaprova
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.minhaprova.databinding.ActivityAcao1Binding
+import com.google.android.material.snackbar.Snackbar
+
+class ActivityAcao1 : AppCompatActivity() {
+    lateinit var binding: ActivityAcao1Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_acao1)
+
+
+        binding.buttonCancel.setOnClickListener {
+            setResult(Activity.RESULT_CANCELED)
+            finish()
+        }
+
+        binding.buttonOk.setOnClickListener {
+            val res = binding.editText.text.toString()
+            if(res != "") {
+                val i = Intent()
+                i.putExtra("RESPOSTA", res)
+                setResult(Activity.RESULT_OK, i)
+                finish()
+            } else {
+                Toast.makeText(this, R.string.erro1, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/example/minhaprova/MainActivity.kt
+++ app/src/main/java/com/example/minhaprova/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.minhaprova
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.minhaprova.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.button1.setOnClickListener {
+            val intent = Intent(this, ActivityAcao1::class.java)
+            startActivityForResult(intent, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 1) {
+            if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(this, "FUCK THIS SHIT", Toast.LENGTH_SHORT).show()
+            } else if (resultCode == Activity.RESULT_OK) {
+                binding.text1.text = data?.getStringExtra("RESPOSTA").toString()
+                binding.invalidateAll()
+            }
+        }
     }
 }
